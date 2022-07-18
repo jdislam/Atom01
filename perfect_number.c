db72aa10e5747a36db63b0e52bd3cd1daa4c6746
MD JAHIDUL ISLAM <105884411+jdislam@users.noreply.github.com>
2022-07-19 00:27:42 +0600

    Learning_Codes

--- perfect_number.c
+++ perfect_number.c
@@ -8,11 +8,19 @@ jodi proper factor er sum == n taile oi n ekta perfect number
 #include<stdio.h>
 int main(void)
 {
-   int i,n;
+   int i,n,sum=0;
    printf("ENTER A NUMBER = ");
    scanf("%d",&n);
 
-
+      for(i=1;i<n;i++)
+      {
+        if(n%i==0)
+        {
+           sum = sum + i;
+        }
+      }
+      if(sum == n) printf("PERFECT NUMBER\n");
+      else printf("NOT A PERFECT NUMBER");
 
   return 0;
 }
