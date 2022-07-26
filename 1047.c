3763db4dbadaf80c1d65162d0a2f3b1b5b4989bb
jdislam <prithonjahid2480@gmail.com>
2022-07-26 10:21:17 +0600

    Update 1047.c

--- 1047.c
+++ 1047.c
@@ -1,14 +1,53 @@
 #include<stdio.h>
 int main()
 {
-  int ini_hour, ini_min, final_hour, final_min;
-  scanf("%d %d %d %d", &ini_hour, &ini_min, &final_hour, &final_min);
+  int a,b,c,d;
+  scanf("%d %d %d %d", &a, &b, &c, &d);
+  if(c>a)
+  {
+    if(d>b)
+    {
+      printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",c-a,d-b);
+    }
+    else if(b>d)
+    {
+      printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",(c-1)-a, (d+60)-b);
+    }
+    else if(b==d)
+    {
+      printf("O JOGO DUROU %d HORA(S) E 0 MINUTO(S)\n", c-a);
+    }
+  }
+  else if(c==a)
+  {
+    if(b==d)
+    {
+      printf("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)\n");
+    }
+    else if(b>d)
+    {
+      printf("O JOGO DUROU 23 HORA(S) E %d MINUTO(S)\n", (d+60)-b);
+    }
+    else if(b<d)
+    {
+      printf("O JOGO DUROU 0 HORA(S) E %d MINUTO(S)\n", d-b);
+    }
+  }
+  else if(c<a)
+  {
+    if(b>d)
+    {
+      printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", ((c-1)+24)-a, (d+60)-b);
+    }
+    else if(b<d)
+    {
+      printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", (c+24)-a, (d-b));
+    }
+    else if(b==d)
+    {
+      printf("O JOGO DUROU %d HORA(S) E 0 MINUTO(S)\n", (c+24)-a);
+    }
+  }
 
-  a = final_hour-final_min;
-  b = ini_min-final_min;
-
-  printf("%d\n %d\n", a,b);
-
-  for()
   return 0;
 }
